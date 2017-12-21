f9a47fc9050b2d0e524813220904ab2a54e1b077
meanfan <meanfan@outlook.com>
2017-12-21 23:08:46 +0800

    update v0.3

--- app/src/main/java/com/mean/meanminesweeper/MainActivity.java
+++ app/src/main/java/com/mean/meanminesweeper/MainActivity.java
@@ -2,10 +2,13 @@ package com.mean.meanminesweeper;
 
 import android.app.*;
 import android.os.*;
+import android.text.InputType;
 import android.view.*;
 import android.view.View.OnClickListener;
 import android.widget.*;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends Activity {
@@ -17,6 +20,10 @@ public class MainActivity extends Activity {
     private GridLayout layout_mineSpace = null;
     private LinearLayout layout_main = null;
     private LinearLayout layout_ctrl = null;
+    private LinearLayout layout_status = null;
+    private TextView tv_time = null;
+    private TextView tv_score = null;
+    private TextView tv_mineNum = null;
     private Button bt_start = null;
     private int rowNum = 16;
     private int colNum = 16;
@@ -24,28 +31,40 @@ public class MainActivity extends Activity {
     private int i;
     private int j;
     private int clickCount = 0;
+    private Timer gameTime = null;
+    private TimerTask task = null;
+    private  int sec = 0;
+    private  int score = 0;
+    private boolean isRoundEnd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super. onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         layout_main = findViewById(R.id.activity_main);
         layout_ctrl = findViewById(R.id.layout_ctrl);
+        layout_status = findViewById(R.id.layout_status);
 
         et_rowNum = findViewById(R.id.et_rowNum);
         et_rowNum.setText(String.format(getString(R.string.str_int),rowNum));
+        et_rowNum.setInputType(InputType.TYPE_CLASS_NUMBER);
+
         et_colNum = findViewById(R.id.et_colNum);
         et_colNum.setText(String.format(getString(R.string.str_int),colNum));
+        et_colNum.setInputType(InputType.TYPE_CLASS_NUMBER);
 
         et_mineNum = findViewById(R.id.et_mineNum);
         et_mineNum.setText(String.format(getString(R.string.str_int),mineNum));
-
+        et_mineNum.setInputType(InputType.TYPE_CLASS_NUMBER);
+        tv_time = findViewById(R.id.tv_time);
+        tv_score = findViewById(R.id.tv_score);
+        tv_mineNum = findViewById(R.id.tv_mineNum);
         bt_start = findViewById(R.id.bt_start);
         bt_start.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                int row = Integer.getInteger(et_rowNum.getText().toString());
-                int col = Integer.getInteger(et_colNum.getText().toString());
-                int num = Integer.getInteger(et_mineNum.getText().toString());
+                int row = Integer.parseInt(et_rowNum.getText().toString());
+                int col = Integer.parseInt(et_colNum.getText().toString());
+                int num = Integer.parseInt(et_mineNum.getText().toString());
                 if(row<3 ||col<3) {
                     Toast.makeText(MainActivity.this, getString(R.string.size_wrong_msg), Toast.LENGTH_SHORT).show();
                     return;
@@ -61,21 +80,52 @@ public class MainActivity extends Activity {
                 rowNum = row;
                 colNum = col;
                 mineNum = num;
-                startGame();
-                et_rowNum.setEnabled(false);
-                et_colNum.setEnabled(false);
-                et_mineNum.setEnabled(false);
-                ((Button)v).setEnabled(false);
+                startRound();
             }
         });
-        //TODO 自定义数据
-
-
-
-
+        layout_ctrl.setVisibility(View.VISIBLE);
+        layout_status.setVisibility(View.INVISIBLE);
     }
-    public void startGame()
+    public void startRound()
     {
+        sec=0;
+        score = 0;
+        clickCount = 0;
+        isRoundEnd = false;
+        task = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        sec++;
+                        int minute = sec/60;
+                        int second = sec%60;
+                        String s_minute;
+                        String s_second;
+                        if(minute/10==0)
+                            s_minute = String.format(getString(R.string.str_0_int),minute);
+                        else
+                            s_minute = String.format(getString(R.string.str_int),minute);
+                        if(second/10==0)
+                            s_second = String.format(getString(R.string.str_0_int),second);
+                        else
+                            s_second = String.format(getString(R.string.str_int),second);
+                        tv_time.setText(s_minute+getString((R.string.time_divider))+s_second);
+                        if(isRoundEnd)
+                            task.cancel();
+                    }
+                });
+            }
+        };
+        gameTime = new Timer();
+        gameTime.schedule(task,1000,1000);
+        tv_score.setText(String.valueOf(0));
+        tv_mineNum.setText(String.valueOf(mineNum));
+        layout_status.setVisibility(View.VISIBLE);
+        ScrollView sv = findViewById(R.id.sv);
+        if(layout_mineSpace != null)
+            sv.removeView(layout_mineSpace);
         layout_mineSpace = new GridLayout(this);
         layout_mineSpace.setColumnCount(colNum);
         b = new Button[rowNum][colNum];
@@ -93,6 +143,8 @@ public class MainActivity extends Activity {
                 layout_mineSpace.addView(b[i][j],params);
                 b[i][j].setOnClickListener(new OnClickListener() {
                     public void onClick(View v) {
+                        if(isRoundEnd)
+                            return;
                         int i,j=0;
                         for(i=0; i< rowNum; i++)
                         {
@@ -132,12 +184,13 @@ public class MainActivity extends Activity {
                             }
                         }
                         else {
-                            showAll();
+                            endRound();
+                            Toast.makeText(MainActivity.this, "失败！", Toast.LENGTH_SHORT).show();
                             //layout_main.removeAllViews();
                         }
                         if(clickCount == rowNum * colNum -mineNum) {
-                            showAll();
-                            //layout_main.removeAllViews();
+                            endRound();
+                            Toast.makeText(MainActivity.this, "胜利！", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
@@ -169,12 +222,21 @@ public class MainActivity extends Activity {
                 });
             }
         }
-        ScrollView sv = findViewById(R.id.sv);
         sv.addView(layout_mineSpace);
+    }
+    private  void endRound()
+    {
+        isRoundEnd = true;
+        showAllMine();
+        layout_ctrl.setVisibility(View.VISIBLE);
+        bt_start.setText("重开");
+        layout_status.setVisibility(View.INVISIBLE);
     }
     private void showMine(int x,int y)
     {
         clickCount++;
+        score++;
+        tv_score.setText(String.format(getString(R.string.str_int),score));
         b[x][y].setText(""+map.get(x,y));
         map.setShowStatus(x,y);
     }
@@ -220,7 +282,7 @@ public class MainActivity extends Activity {
         if(map.get(i,j)!=-1)
             showMine(i, j);
     }
-    private void showAll(){
+    private void showAllMine(){
         for(int i = 0; i< rowNum; i++)
             for(int j = 0; j< colNum; j++)
             {
@@ -230,9 +292,7 @@ public class MainActivity extends Activity {
                 else if(v==0)
                     b[i][j].setText("0");
                 else
-                {
-                    b[i][j].setText(""+v);
-                }
+                    b[i][j].setText(String.format(getString(R.string.str_int),v));
             }
     }
     private class Map {
